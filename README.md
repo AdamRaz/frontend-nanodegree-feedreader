55e20f69510c3d17f1b7e4d3a5cc1d370e39856e
Adam Razzak <a.abdel@hotmail.co.uk>
2018-06-07 00:33:00 +0100

    readme and final comments

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Project instructions
+
+just open index.html in a browser (try chrome if possible) and observe the jasmine test output at the bottom of the page
+
+
 # Project Overview
 
 In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,7 +72,7 @@ $(function() {
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
-          */    
+          */
             it('menu changes visibility when the menu icon is clicked', function() {
                 let bodyClasses = document.body.classList;
                 let menuIcon = $('.menu-icon-link');
@@ -84,7 +84,7 @@ $(function() {
             });
         });
 
-        
+
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
@@ -99,12 +99,13 @@ $(function() {
                 // AR - executes async code before each test in this test suite scope and uses done to signal completion
                 loadFeed(1, function() {
                     done()
-                });
                 // AR - calling done in the callback provided for in the loadFeed function... all async functions should really have callbacks?
-                // AR - tried this with just done() without wrapping it in function () {} first... did not work as expected, later code loggin innerHTML did not work
+                // AR - tried this with just done() without wrapping it in function () {} first... did not work as expected, later code accessing innerHTML did not have expected value
+                });
             });
 
             it('entries exist within feed when feed load completes', function(done) {
+                // AR - passing in 'done' as function parameter and using done() at the end lets jasmine framework know this test relies on asynchronous code
                 let feedEntries = document.querySelector('.feed').innerHTML;
                 expect(feedEntries).not.toBe(null);
                 done();
@@ -122,13 +123,12 @@ $(function() {
         let feedEntry1;
         let feedEntry2;
             beforeEach(function(done) {
-                
-                // AR -executes async code before each test in this test suite scope and uses done to signal completion
+                // AR - executes async code before each test in this test suite scope and uses done to signal completion
                 loadFeed(1, function() {
                     feedEntry1 = document.querySelector('.feed').innerHTML;
                     done()
                 });
-                // Ar - we execute loadfeed with different id parameter and extract the result from HTML for comparison later
+                // AR - we execute loadfeed with different id parameter and extract the result from HTML for comparison later
                 loadFeed(2, function() {
                     feedEntry2 = document.querySelector('.feed').innerHTML;
                     done()
